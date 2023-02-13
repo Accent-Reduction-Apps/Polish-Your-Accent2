9b04122d8d7a6306dec7b72e3f9c809913ef4767
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-13 15:46:28 +0100

    Revert "started with npm create-react-app"
    This reverts commit 9fa44830fe68315e45177c990a7438e6300e684e.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home";
+import Layout from "./Pages/Layout";
+import Demo from "./Pages/Demo";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import NoPage from "./Pages/NoPage";
+import RegisterHere from "./Pages/RegisterHere";
+import ErrorPage from "./Pages/ErrorPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path="about" element={<About/>}/>
+                    <Route path="contact" element={<Contact/>}/>
+                    <Route path="registration" element={<RegisterHere/>}/>
+                    <Route path="registration" element={<RegisterHere/>}/>
+                    <Route path="demo" element={<Demo/>}/>
+                    <Route path="*" element={<NoPage/>}/>
+                    <Route errorElement={<ErrorPage/>}/>
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
