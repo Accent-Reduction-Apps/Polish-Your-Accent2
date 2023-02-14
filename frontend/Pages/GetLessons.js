f3968b41c48b944bd632b5e1e37d8b8e02443f1a
MHN468 <sebastian.sosin@motorolasolutions.com>
2023-02-14 10:44:53 +0100

    Clear double source.

--- frontend/Pages/GetLessons.js
+++ frontend/Pages/GetLessons.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './GetLessons.css';
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 
 const GetLessonsTable = () => {
     const [data, setData] = useState([]);
