edc41ea1be8f54be2465e235eb91b9c60ec02514
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-14 11:33:31 +0100

    Link is passing state to child (LessonById)

--- frontend/src/Pages/GetLessons.jsx
+++ frontend/src/Pages/GetLessons.jsx
@@ -4,32 +4,48 @@ import { Link } from "react-router-dom";
 import '../GetLessons.css';
 
 const GetLessons = () => {
-    const [data, setData] = useState([]);
+    const [lessons, setLessons] = useState([]);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
+
+
     useEffect(() => {
-        async function fetchData() {
-            setIsLoading(true);
-            setError(null);
-
-            try {
-                const response = await fetch("http://localhost:8080/lessons");
-
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
-
-                const json = await response.json();
-                setData(json);
-            } catch (e) {
-                setError(e.message);
-            } finally {
-                setIsLoading(false);
-            }
+        const getData = async () => {
+            const lessons = await fetchData();
+            setLessons(lessons);
         }
-        fetchData();
+        getData();
     }, []);
+
+    const fetchData = async () => {
+        // setIsLoading(true);
+        // setError(null);
+        //
+        // try {
+        //     const response = await fetch("http://localhost:8080/lessons");
+        //
+        //     if (!response.ok) {
+        //         throw new Error(response.statusText);
+        //     }
+        //
+        //     const json = await response.json();
+        //     setData(json)
+        //     console.log(json)
+        //     return json;
+        //
+        // } catch (e) {
+        //     setError(e.message);
+        // } finally {
+        //     setIsLoading(false);
+        // }
+
+        const response = await fetch ('http://localhost:8080/lessons');
+        const data = await response.json();
+        return data;
+    }
+
+
 
     if (isLoading) {
         return <p>Loading...</p>;
@@ -49,14 +65,15 @@ const GetLessons = () => {
             </tr>
             </thead>
             <tbody>
-            {data.map(item => (
-                <Link to={`/lesson/${item.id}`} key={item.id}>
-                    <tr>
-                        <td>{item.id}</td>
-                        <td>{item.topic}</td>
-                        <td>{item.text}</td>
-                    </tr>
-                </Link>
+            {lessons.map((item) => (
+                <tr key={item.id}>
+                    <td>
+                        <Link to={`/lesson/${item.id}`} state={item} >
+                            {item.id}</Link>
+                    </td>
+                    <td>{item.topic}</td>
+                    <td>{item.text}</td>
+                </tr>
             ))}
             </tbody>
         </table>
