6a62571c15a23986c8b2edcd3cad78886b8ff844
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-14 11:37:56 +0100

    GetLessonsById is controlled by props object (lesson)

--- frontend/src/Pages/GetLessonById.jsx
+++ frontend/src/Pages/GetLessonById.jsx
@@ -1,36 +1,68 @@
 import React, {useEffect, useState} from "react";
+import {useLocation} from "react-router";
+import '../GetLessons.css';
 
-const GetLessonById = ({match}) => {
-    console.log(match.params.lessonId);
+const GetLessonById = (props) => {
+    const lessonId = useLocation();
+    const id = lessonId.state.id;
     const [lesson, setLesson] = useState({});
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        async function fetchData() {
-            setIsLoading(true);
-            setError(null);
+        // async function fetchData() {
+        //     setIsLoading(true);
+        //     setError(null);
+        //
+        //     try {
+        //         const response = await fetch(
+        //             `http://localhost:8080/lessons/${id}`
+        //         );
+        //         console.log(response.json())
+        //
+        //         if (!response.ok) {
+        //             throw new Error(response.statusText);
+        //         }
+        //
+        //         const json = await response.json();
+        //         setLesson(json);
+        //     } catch (e) {
+        //         setError(e.message);
+        //     } finally {
+        //         setIsLoading(false);
+        //     }
+        // }
+        const getData = async () => {
+            const dataa = await fetchData();
+            setLesson(dataa);
+        }
+        getData();
+    }, []);
 
-            try {
-                const response = await fetch(
-                    `http://localhost:8080/lessons/${match.params.lessonId}`
-                );
 
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
+    const fetchData = async() => {
+        setIsLoading(true);
+        setError(null);
 
-                const json = await response.json();
-                setLesson(json);
-            } catch (e) {
-                setError(e.message);
-            } finally {
-                setIsLoading(false);
+        try {
+            const response = await fetch(
+                `http://localhost:8080/lessons/${id}`
+            );
+
+            if (!response.ok) {
+                throw new Error(response.statusText);
             }
+
+            const json = await response.json();
+            return json;
+            // setLesson(json);
+        } catch (e) {
+            setError(e.message);
+        } finally {
+            setIsLoading(false);
         }
 
-        fetchData();
-    }, [match.params.lessonId]);
+    }
 
     if (isLoading) {
         return <p>Loading...</p>;
@@ -39,7 +71,7 @@ const GetLessonById = ({match}) => {
     if (error) {
         return <p>An error occurred: {error}</p>;
     }
-    console.log(lesson.text);
+    //console.log(lesson.text);
     return (
         <div>
             <h2>LESSON</h2>
