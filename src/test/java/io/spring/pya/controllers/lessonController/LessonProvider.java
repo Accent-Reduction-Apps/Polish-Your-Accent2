abcb22fdd83ddf4c96d6b24552cee505a03f02a4
GasiorMateusz <mateuszgasior314@gmail.com>
2023-02-15 09:50:22 +0100

    PYA-106 add createLessonList

--- src/test/java/io/spring/pya/controllers/lessonController/LessonProvider.java
+++ src/test/java/io/spring/pya/controllers/lessonController/LessonProvider.java
@@ -3,14 +3,24 @@ package io.spring.pya.controllers.lessonController;
 import io.spring.pya.entities.Lesson;
 import io.spring.pya.util.UtilRandomNumber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LessonProvider {
     public static Lesson createRandomLesson() {
-//        return new Lesson(
         return new Lesson(
                 UtilRandomNumber.getRandomLong(),
                 "Random lesson content" + UtilRandomNumber.getRandomLong(),
                 "Random topic" + UtilRandomNumber.getRandomLong()
         );
+    }
+
+    public static List<Lesson> createRandomLessonsList(int size) {
+        List<Lesson> lessons = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            lessons.add(createRandomLesson());
+        }
+        return lessons;
     }
 
     public static Long getRandomId() {
