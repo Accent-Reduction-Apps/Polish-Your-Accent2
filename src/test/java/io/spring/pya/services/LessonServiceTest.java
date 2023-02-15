fce2925448af4e5be57ef6a514f4fe0019a5ab33
GasiorMateusz <mateuszgasior314@gmail.com>
2023-02-15 13:26:38 +0100

    PYA-116 add LessonServiceTest methods to all scenarios

---
+++ src/test/java/io/spring/pya/services/LessonServiceTest.java
@@ -0,0 +1,39 @@
+package io.spring.pya.services;
+
+import org.junit.jupiter.api.Test;
+
+class LessonServiceTest {
+
+    @Test
+    void getAllLessons_loadLessonsFromDB_ListOfLessons() {
+    }
+
+    @Test
+    void getLessonById_LessonExists_Lesson() {
+    }
+
+    @Test
+    void getLessonById_LessonDoesntExists_null() {
+
+    }
+
+    @Test
+    void addLesson_completeLesson_lessonSaved() {
+    }
+
+    @Test
+    void updateLesson_lessonToUpdateExists_updatedLesson() {
+    }
+
+    @Test
+    void updateLesson_lessonToUpdateDoesntExists_null() {
+    }
+
+    @Test
+    void deleteLessonById_lessonToBeDeletedExists_true() {
+    }
+
+    @Test
+    void deleteLessonById_lessonToBeDeletedDoesntExists_false() {
+    }
+}
