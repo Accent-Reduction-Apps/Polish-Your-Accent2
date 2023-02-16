df7a401bcc9bfdefb4243b35c44dc42c08ebc32c
GasiorMateusz <mateuszgasior314@gmail.com>
2023-02-16 10:45:21 +0100

    PYA-110 add lesson controller loggers

--- src/main/java/io/spring/pya/controllers/LessonController.java
+++ src/main/java/io/spring/pya/controllers/LessonController.java
@@ -4,6 +4,8 @@ package io.spring.pya.controllers;
 import io.spring.pya.entities.Lesson;
 import io.spring.pya.exceptions.ResourceNotFoundException;
 import io.spring.pya.services.LessonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/lessons")
 public class LessonController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LessonController.class);
     LessonService lessonService;
 
 
@@ -23,6 +27,7 @@ public class LessonController {
 
     @GetMapping
     public ResponseEntity<List<Lesson>> getAllLessons() {
+        LOGGER.info("get all lessons");
         return new ResponseEntity<>(lessonService.getAllLessons(), HttpStatus.OK);
     }
 
@@ -30,26 +35,29 @@ public class LessonController {
     public ResponseEntity<Lesson> getLessonById(@PathVariable("lessonId") Long id) {
         Lesson foundLesson = lessonService.getLessonById(id);
         if (foundLesson != null) {
+            LOGGER.info("get lesson : " + id);
             return new ResponseEntity<>(foundLesson, HttpStatus.OK);
         } else {
             throw new ResourceNotFoundException("Lesson", id);
         }
-
     }
 
     @PostMapping()
     public void addLesson(@RequestBody Lesson lesson) {
+        LOGGER.info("Add lesson :\n" + lesson);
         lessonService.addLesson(lesson);
     }
 
     @PutMapping("/{lessonId}")
     public ResponseEntity<Lesson> updateLesson(@PathVariable("lessonId") Long id, @RequestBody Lesson lessonNew) throws ResourceNotFoundException {
-        if (lessonService.getLessonById(id) != null) {
+        Lesson oldLesson = lessonService.getLessonById(id);
+        if (oldLesson != null) {
             Lesson updatedLesson = lessonService.updateLesson(id, lessonNew);
             if (updatedLesson == null) {
                 //only possible if save fail
                 throw new ResourceNotFoundException("Lesson", id);
             }
+            LOGGER.info("Update lesson " + oldLesson + "\nwith\n" + lessonNew);
             return new ResponseEntity<>(updatedLesson, HttpStatus.OK);
         } else {
             throw new ResourceNotFoundException("Lesson", id);
@@ -59,6 +67,7 @@ public class LessonController {
     @DeleteMapping("/{lessonId}")
     public ResponseEntity<Lesson> deleteLesson(@PathVariable("lessonId") Long id) {
         if (lessonService.deleteLessonById(id)) {
+            LOGGER.info("Delete lesson with id : " + id);
             return new ResponseEntity<>(HttpStatusCode.valueOf(204));
         } else {
             throw new ResourceNotFoundException("Lesson", id);
--- src/main/java/io/spring/pya/controllers/ResourceNotFoundControllerAdvice.java
+++ src/main/java/io/spring/pya/controllers/ResourceNotFoundControllerAdvice.java
@@ -1,6 +1,8 @@
 package io.spring.pya.controllers;
 
 import io.spring.pya.exceptions.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class ResourceNotFoundControllerAdvice {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceNotFoundControllerAdvice.class);
+
     @ResponseBody
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException exception) {
+        LOGGER.error(exception.getMessage());
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
