a67d1dbf4d3be4786e63989566584e7981e5769c
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-16 09:28:54 +0100

    Minor layout changes continued

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -6,12 +6,14 @@ export default function About() {
     return (
         <div className='bg-warning p-3'>
             <div className='d-flex align-items-center'>
-                <div className='col-md-10'>
-            <h1>tu moze byc np opis dla kogo i po co ta apka</h1>
+                <div className='col-md-8'>
+                    <h2>Think about it:</h2><br/>
+            <h3>People with regional accents on average can increase their wages by 20% and increase their probability of promotion by reducing their accents according to researchers*</h3>
                     <h3>trololo</h3>
-                    <h4>lolo</h4>
+                    <p><br/>*The Wage Penalty of Regional Accents; Grogger, Steinmayr, Winter; National Bureau of Economic Research; January 2020</p>
+                    <p>**Want to earn money? Polish your accent; Lizzy Burden; The Telegraph; 2020.02.12</p>
                 </div>
-                <div className='col-md-2 d-flex justify-content-end'>
+                <div className='col-md-4 d-flex justify-content-end'>
             <img src={MRI} alt='loading...' />
                 </div>
             </div>
