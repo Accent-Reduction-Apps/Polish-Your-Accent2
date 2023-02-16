5e3119188c6d10926897d8a2647813ff0b568f9c
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-16 13:04:02 +0100

    PYA-118 Buttons: added getter and setter to auth export

--- frontend/src/auth.jsx
+++ frontend/src/auth.jsx
@@ -1,3 +1,9 @@
-export let isAuthorized = true
+let authorized = true;
 
-;
+export function isAuthorized() {
+    return authorized;
+}
+
+export function setAuthorized(value) {
+    authorized = value;
+}
