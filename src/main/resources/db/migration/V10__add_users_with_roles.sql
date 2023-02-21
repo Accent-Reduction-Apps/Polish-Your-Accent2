c7918e44f2fa9518ff4a34efff332791322998fc
GasiorMateusz <mateuszgasior314@gmail.com>
2023-02-21 11:57:33 +0100

    PYA-109 encode users password, remove 3 sample users from db

--- src/main/resources/db/migration/V10__add_users_with_roles.sql
+++ src/main/resources/db/migration/V10__add_users_with_roles.sql
@@ -5,28 +5,20 @@ from users;
 
 INSERT INTO users
 (name, email_address, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, role)
-VALUES ('Dawid Deszcz', 'emailDawid@gmail.com', 'hasloDawid', true, true, true, true, 'STUDENT');
+VALUES ('user2', 'user2@gmail.com', '$2a$10$kXe8lnESi50/OSGyxzujpONjX.zFOcjLrO7HYsI/0pxXrd3bP6owe', true, true, true,
+        true, 'STUDENT');
 
 INSERT INTO users
 (name, email_address, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, role)
-VALUES ('Sebastian Sosin', 'emailSebastian@gmail.com', 'hasloSebastian', true, true, true, true, 'STUDENT');
+VALUES ('user', 'user@gmail.com', '$2a$10$n9qzKpeOILMSxyAP.rirO.FFmAI/Kk6TnWHBoKzFLJ6oO9q0fBnTe', true, true, true,
+        true, 'STUDENT');
 
 INSERT INTO users
 (name, email_address, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, role)
-VALUES ('Mateusz Gasior', 'emailMateusz@gmail.com', 'hasloMateusz', true, true, true, true, 'STUDENT');
+VALUES ('admin', 'admin@gmail.com', '$2a$10$E3N.QMGGmzCOGKavhiXhSeH4r0RgyG38s8VOi2b/SclKmO1psYA8C', true, true, true,
+        true, 'ADMIN');
 
 INSERT INTO users
 (name, email_address, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, role)
-VALUES ('Marcin Szuwalski', 'emailMarcin@gmail.com', 'hasloMarcin', true, true, true, true, 'STUDENT');
-
-INSERT INTO users
-(name, email_address, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, role)
-VALUES ('user', 'user@gmail.com', 'userPass', true, true, true, true, 'STUDENT');
-
-INSERT INTO users
-(name, email_address, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, role)
-VALUES ('admin', 'admin@gmail.com', 'adminPass', true, true, true, true, 'ADMIN');
-
-INSERT INTO users
-(name, email_address, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, role)
-VALUES ('staff', 'staff@gmail.com', 'staffPass', true, true, true, true, 'STAFF');
+VALUES ('staff', 'staff@gmail.com', '$2a$10$o0E4XlT.ZYMCUv2m13E2E.tSkyzvuLGy2zuiJSJhYnxUP.CkPjzHa', true, true, true,
+        true, 'STAFF');
