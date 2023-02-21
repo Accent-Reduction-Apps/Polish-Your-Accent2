1846ae776fb995de440f8a1c5d025034827b428b
GasiorMateusz <mateuszgasior314@gmail.com>
2023-02-21 09:13:13 +0100

    PYA-109 change permissions

--- src/main/java/io/spring/pya/security/UserPermission.java
+++ src/main/java/io/spring/pya/security/UserPermission.java
@@ -1,18 +1,18 @@
 package io.spring.pya.security;
 
 public enum UserPermission {
-    PRIVACY("privacy:read"),
-    DETAILS("details:read"),
-    SEARCH("search:write"),
-    READER("items:read"),
-    EDITOR("items:write");
+    LESSONS_READ("lessons:read"),
+    LESSONS_WRITE("lessons:write"),
+    LESSONS_DELETE("lessons:delete"),
+    USERS_READ("users:read"),
+    USERS_WRITE("users:write"),
+    USERS_DELETE("users:delete");
 
     private final String permission;
 
     UserPermission(String permission) {
         this.permission = permission;
     }
-
 
     public String getPermission() {
         return permission;
