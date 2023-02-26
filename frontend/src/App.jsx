60b17813dd78a0a3167e331ac28133d0cdaffedf
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-26 18:03:53 +0100

    Wrapped entire Router with the AppContext.Provider component and set the default value of the context to an object with a boolean property named isUserAuthorized and a setter function named setIsUserAuthorized,optimized imports, removed unused functions and variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,45 @@
 import React, {useState} from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {AuthorizationContext} from "./auxiliary/AuthorizationContext";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './Pages/Home';
-import Layout from './Pages/Layout';
-import Lessons from './Pages/Lessons';
-import About from './Pages/About';
-import TeamSlotherin from './Pages/TeamSlotherin';
-import NoPage from './Pages/NoPage';
-import RegisterHere from './Pages/RegisterHere';
-import ErrorPage from './Pages/ErrorPage';
-import GetLessonById from './Pages/GetLessonById';
-import GetLessons from './Pages/GetLessons';
-import GetUser from './Pages/GetUser';
-import Login from "./Login";
-import Logout from "./Pages/Logout";
-import { isAuthorized, setAuthorized } from './auth';
+import Home from './pages/Components/Landing/Home';
+import Layout from './pages/Navigation/Layout';
+import Lessons from './pages/Components/LessonList/Lessons';
+import About from './pages/Components/About/About';
+import TeamSlotherin from './pages/Components/About/TeamSlotherin';
+import NoPage from './pages/ErrorPages/NoPage';
+import RegisterHere from './pages/Components/Register/RegisterHere';
+import ErrorPage from './pages/ErrorPages/ErrorPage';
+import GetLessonById from './pages/Components/Lesson/GetLessonById';
+import GetLessons from './pages/Components/LessonList/GetLessons';
+import GetUser from './pages/Components/User/GetUser';
+import Logout from "./pages/Components/Logout/Logout";
+import Auxiliary from "./pages/Components/Auxiliary/Auxiliary";
+import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 document.title = 'Polish Your Accent';
 
 export default function App() {
 
-    const [isAuthorizedState, setIsAuthorizedState] = useState(isAuthorized());
-
-    function handleLogout() {
-        setAuthorized(false);
-        setIsAuthorizedState(false);
-    }
-
-
-    return (
+    const [isUserAuthorized, setIsUserAuthorized] = useState(false);
+    return (<AuthorizationContext.Provider value={[isUserAuthorized, setIsUserAuthorized]}>
         <Router>
             <Routes>
-
-                <Route path='/' element={<Layout isAuthorized={isAuthorizedState} onLogout={handleLogout} />}>
+                <Route path='/' element={<Layout/>}>
                     <Route index element={<Home/>}/>
                     <Route path='about' element={<About/>}/>
-                    <Route path='teamInfo' element={<TeamSlotherin/>}/>
+                    <Route path='teaminfo' element={<TeamSlotherin/>}/>
                     <Route path='registration' element={<RegisterHere/>}/>
                     <Route path='logout' element={<Logout/>}/>
                     <Route path='demo' element={<Lessons/>}/>
                     <Route exact path='/lessons' element={<GetLessons/>}/>
                     <Route path='/lesson/:lessonId' element={<GetLessonById/>}/>
                     <Route path='/my-account' element={<GetUser/>}/>
+                    <Route path='/auxiliary' element={<Auxiliary/>}/>
                     <Route path='*' element={<NoPage/>}/>
                     <Route errorElement={<ErrorPage/>}/>
-
                 </Route>
             </Routes>
-        </Router>);
-
+        </Router>
+    </AuthorizationContext.Provider>);
 }
