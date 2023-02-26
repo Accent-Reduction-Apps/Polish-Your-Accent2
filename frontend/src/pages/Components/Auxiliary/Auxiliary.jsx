e235eb874134a5a407942895d69df560a2a9d45d
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-26 18:08:52 +0100

    Added auxiliary component to test dymamic navbar

---
+++ frontend/src/pages/Components/Auxiliary/Auxiliary.jsx
@@ -0,0 +1,26 @@
+import React, {useContext} from 'react';
+import {Button} from "react-bootstrap";
+import {AuthorizationContext} from "../../../auxiliary/AuthorizationContext";
+import '../../../styles/Common.css';
+
+export default function Auxiliary() {
+    const [isUserAuthorized, setIsUserAuthorized] = useContext(AuthorizationContext);
+
+    const handleClick = () => {
+        setIsUserAuthorized(!isUserAuthorized);
+    };
+
+    return (
+        <div className='bg-warning p-3'>
+            <h1 className='fade-in'>AUXILIARY</h1>
+            <h4 className='slide-in'>dummy login/logoff button</h4>
+            <h6 className='rotate-in'>for testing purpose only</h6>
+            <p>changes text&functione</p>
+            <Button onClick={handleClick}
+                    variant={isUserAuthorized ? 'danger' : 'dark'}>{isUserAuthorized ? 'LOG OUT' : 'LOG IN'}</Button>
+            <br /><br />
+            <div className={!isUserAuthorized ? 'text-fading' : 'shake'}>{isUserAuthorized ? 'GREAT SUCCESS' : 'All work and no play makes Jack a dull boy.'}</div>
+        </div>
+    );
+}
+
