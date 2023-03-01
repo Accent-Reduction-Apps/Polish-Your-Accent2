250c1005dde9cb60105af722509067052527d4dc
Dawid Deszcz <d.deszcz@gmail.com>
2023-03-01 16:30:28 +0100

    PYA-168 Lesson 7 to 18 added

---
+++ src/main/resources/db/migration/V18__update_and_create_lessons.sql
@@ -0,0 +1,21 @@
+INSERT INTO lessons (topic, text)
+VALUES ('Transkrypcja fonetyczna a ortografia', 'Lesson content to be updated');
+
+INSERT INTO lessons (topic, text)
+VALUES ('Z czego składa się sylaba? Co to jest fonotaktyka?', 'Lesson content to be updated');
+
+INSERT INTO lessons (topic, text)
+VALUES ('Wymowa i zapis fonetyczny dłuższych wyrazów. Akcent
+wyrazowy: sylaby akcentowane i nieakcentowane', 'Lesson content to be updated');
+
+INSERT INTO lessons (topic, text)
+VALUES ('Redukcja sylaby nieakcentowanej', 'Lesson content to be updated');
+
+INSERT INTO lessons (topic, text)
+VALUES ('Czy angielski akcent wyrazowy jest całkowicie nieprzewidywalny?', 'Lesson content to be updated');
+
+INSERT INTO lessons (topic, text)
+VALUES ('Czy angielski akcent wyrazowy jest całkowicie nieprzewidywalny?', 'Lesson content to be updated');
+
+INSERT INTO lessons (topic, text)
+VALUES ('Dźwięczność głosek — fakty i złudzenia', 'Lesson content to be updated');
