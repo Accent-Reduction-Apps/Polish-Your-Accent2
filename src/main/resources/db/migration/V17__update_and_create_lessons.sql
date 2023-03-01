6daa41124d81d21a32ba058ee65ffceb22ce4c7e
Dawid Deszcz <d.deszcz@gmail.com>
2023-03-01 16:40:07 +0100

    PYA-168 Update lessons input so that content is in line with previous additions

--- src/main/resources/db/migration/V17__update_and_create_lessons.sql
+++ src/main/resources/db/migration/V17__update_and_create_lessons.sql
@@ -1,23 +1,23 @@
 UPDATE lessons
 SET	topic = 'Klasyfikacja spółgłosek',
-    text = 'Lesson content to be updated'
+    text = '<DIV>Lesson content to be updated</DIV>'
 WHERE lesson_id = 7;
 
 UPDATE lessons
 SET	topic = 'Spółgłoski angielskie: jak zapamiętać ich symbole?',
-       text = 'Lesson content to be updated'
+       text = '<DIV>Lesson content to be updated</DIV>'
 WHERE lesson_id = 8;
 
 UPDATE lessons
 SET	topic = 'Najważniejsze właściwości samogłosek',
-       text = 'Lesson content to be updated'
+       text = '<DIV>Lesson content to be updated</DIV>'
 WHERE lesson_id = 9;
 
 UPDATE lessons
 SET	topic = 'Samogłoski angielskie i ich zapis fonetyczny',
-       text = 'Lesson content to be updated'
+       text = '<DIV>Lesson content to be updated</DIV>'
 WHERE lesson_id = 10;
 
 INSERT INTO lessons (topic, text)
-VALUES ('Angielskie dyftongi', 'Lesson content to be updated');
+VALUES ('Angielskie dyftongi', '<DIV>Lesson content to be updated</DIV>');
 
--- src/main/resources/db/migration/V18__update_and_create_lessons.sql
+++ src/main/resources/db/migration/V18__update_and_create_lessons.sql
@@ -1,21 +1,21 @@
 INSERT INTO lessons (topic, text)
-VALUES ('Transkrypcja fonetyczna a ortografia', 'Lesson content to be updated');
+VALUES ('Transkrypcja fonetyczna a ortografia', '<DIV>Lesson content to be updated</DIV>');
 
 INSERT INTO lessons (topic, text)
-VALUES ('Z czego składa się sylaba? Co to jest fonotaktyka?', 'Lesson content to be updated');
+VALUES ('Z czego składa się sylaba? Co to jest fonotaktyka?', '<DIV>Lesson content to be updated</DIV>');
 
 INSERT INTO lessons (topic, text)
 VALUES ('Wymowa i zapis fonetyczny dłuższych wyrazów. Akcent
-wyrazowy: sylaby akcentowane i nieakcentowane', 'Lesson content to be updated');
+wyrazowy: sylaby akcentowane i nieakcentowane', '<DIV>Lesson content to be updated</DIV>');
 
 INSERT INTO lessons (topic, text)
-VALUES ('Redukcja sylaby nieakcentowanej', 'Lesson content to be updated');
+VALUES ('Redukcja sylaby nieakcentowanej', '<DIV>Lesson content to be updated</DIV>');
 
 INSERT INTO lessons (topic, text)
-VALUES ('Czy angielski akcent wyrazowy jest całkowicie nieprzewidywalny?', 'Lesson content to be updated');
+VALUES ('Czy angielski akcent wyrazowy jest całkowicie nieprzewidywalny?', '<DIV>Lesson content to be updated</DIV>');
 
 INSERT INTO lessons (topic, text)
-VALUES ('Czy angielski akcent wyrazowy jest całkowicie nieprzewidywalny?', 'Lesson content to be updated');
+VALUES ('Czy angielski akcent wyrazowy jest całkowicie nieprzewidywalny?', '<DIV>Lesson content to be updated</DIV>');
 
 INSERT INTO lessons (topic, text)
-VALUES ('Dźwięczność głosek — fakty i złudzenia', 'Lesson content to be updated');
+VALUES ('Dźwięczność głosek — fakty i złudzenia', '<DIV>Lesson content to be updated</DIV>');
